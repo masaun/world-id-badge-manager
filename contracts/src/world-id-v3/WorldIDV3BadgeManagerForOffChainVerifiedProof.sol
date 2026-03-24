a7bcfd661abe918925b8b074daf8c232c7213972
masaun <masaun@protonmail.com>
2026-03-24 16:41:37 +0900

    feat/SC: Define the WorldIDV3BadgeManagerForOffChainVerifiedProof.sol

---
+++ contracts/src/world-id-v3/WorldIDV3BadgeManagerForOffChainVerifiedProof.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+/**
+ * @title - WorldIDV3BadgeManager contract for the Off-chain verified proof
+ */
+contract WorldIDV3BadgeManagerForOffChainVerifiedProof {
+
+    constructor() {}
+
+}
